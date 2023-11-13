d0282b216072b89c0fa37cf8a1dd25904a9690f1
Javier Guerrero <javigh87@gmail.com>
2023-11-13 07:59:03 +0100

    refactor: Country interfaces modified

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,3 +3,22 @@ import { ReactNode } from "react";
 export interface PropsWithChildren {
   children: ReactNode;
 }
+
+export interface Country {
+  capital: string;
+  population: number;
+  region: string;
+  flag: string;
+}
+
+export interface ApiCountry {
+  capital: string;
+  population: number;
+  region: string;
+  flags: Flag;
+}
+
+interface Flag {
+  png: string;
+  svg: string;
+}
