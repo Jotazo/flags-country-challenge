e5f6d6440a52ea928abdb2c5b5527d56d49930b4
Javier Guerrero <javigh87@gmail.com>
2023-11-13 08:00:05 +0100

    feature: Countries service handle return value

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,7 +1,14 @@
+import { ApiCountry, Country } from "../interfaces";
+
 const API_URL = "https://restcountries.com/v3.1";
 
-export const getAllCountries = async () => {
+export const getAllCountries = async (): Promise<Country[]> => {
   const response = await fetch(`${API_URL}/all`);
   const data = await response.json();
-  return data;
+  return data.map((country: ApiCountry) => ({
+    capital: country.capital,
+    population: country.population,
+    region: country.region,
+    flag: country.flags.png,
+  }));
 };
