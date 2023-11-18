ae61574d5d672666a6e69f0b6b91b50dd0d19839
Javier Guerrero <javigh87@gmail.com>
2023-11-18 08:13:23 +0100

    feature: Added container classes to pages

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,11 +7,13 @@ import "./Layout.css";
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <header className="flex justify-between items-center bg-white dark:bg-darkBlue py-6 px-4 transition duration-500 layout-header">
-        <h3 className="text-sm font-extrabold text-darkBlueLightText dark:text-white">
-          Where in the world?
-        </h3>
-        <ThemeToggler variant="animated" />
+      <header className=" bg-white dark:bg-darkBlue py-6 px-4 transition duration-500 layout-header">
+        <div className="container flex justify-between items-center mx-auto">
+          <h3 className="text-sm font-extrabold text-darkBlueLightText dark:text-white">
+            Where in the world?
+          </h3>
+          <ThemeToggler variant="animated" />
+        </div>
       </header>
       <main className="bg-lightGray dark:bg-darkBlueBg h-[calc(100vh-80px)] transition duration-500">
         {children}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 const MainPage = ({ countries }) => {
   return (
-    <div className="px-4 pt-4">
+    <div className="container px-4 pt-4 mx-auto">
       <Header />
     </div>
   );
