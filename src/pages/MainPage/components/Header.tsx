c5df92a8499e6fd5f88be12213b07d3c5dbccfec
Javier Guerrero <javigh87@gmail.com>
2023-11-18 07:52:27 +0100

    feature: Header added

--- src/pages/MainPage/components/Header.tsx
+++ src/pages/MainPage/components/Header.tsx
@@ -1,11 +1,13 @@
-import SearchField from "./SearchField"
+import CountrySelector from "./CountrySelector";
+import SearchField from "./SearchField";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex flex-wrap sm:flex-nowrap">
       <SearchField />
+      <CountrySelector />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
