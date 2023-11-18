eb9ebe5fbeb4f026fc47b8e69e4dfcd51995e8ea
Javier Guerrero <javigh87@gmail.com>
2023-11-18 22:07:34 +0100

    feature: Added regions to appStore

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -5,20 +5,28 @@ import { getAllCountries } from "../services/countries";
 interface AppState {
   countries: Country[];
   countrySelected: Country | null;
+  regions: string[];
   searchField: string;
   regionFilter: string;
   getCountries: () => Promise<void>;
+  setRegionFilterSelected: (region: string) => void;
 }
 
+// TODO: Handle Loading/Error state fetching
 const useAppStore = create<AppState>()((set) => ({
   countries: [],
   countrySelected: null,
+  regions: [],
   searchField: "",
   regionFilter: "",
   getCountries: async () => {
     const countries = await getAllCountries();
-    set({ countries });
+    const regions = [
+      ...new Set(countries.map((country) => country.region)),
+    ].sort();
+    set({ countries, regions });
   },
+  setRegionFilterSelected: (region) => set({ regionFilter: region }),
 }));
 
 export default useAppStore;
