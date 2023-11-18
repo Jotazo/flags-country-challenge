8b9de17762a10135f17788d7ce3fb20f905825ad
Javier Guerrero <javigh87@gmail.com>
2023-11-18 08:12:33 +0100

    styles: ChevronIcon modified width & fill

--- src/assets/Icons/ChevronDownIcon.tsx
+++ src/assets/Icons/ChevronDownIcon.tsx
@@ -2,16 +2,15 @@ const ChevronDownIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="10"
-      height="10"
+      className="w-3"
       viewBox="0 0 10 10"
       fill="none"
     >
       <path
+        className="dark:fill-white fill-black"
         fillRule="evenodd"
         clipRule="evenodd"
         d="M7.875 2.875L5 5.75L2.125 2.875L1.25 3.75L5 7.5L8.75 3.75L7.875 2.875Z"
-        fill="white"
       />
     </svg>
   );
