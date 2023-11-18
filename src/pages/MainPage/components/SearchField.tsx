a8cf6482b72464bf7c3babf65e647b247d9abff5
Javier Guerrero <javigh87@gmail.com>
2023-11-18 07:51:31 +0100

    styles: Searchfield modified with input styles

--- src/pages/MainPage/components/SearchField.tsx
+++ src/pages/MainPage/components/SearchField.tsx
@@ -1,17 +1,15 @@
 import { SearchIcon } from "../../../assets/Icons";
 
-import "./SearchField.css";
-
 const SearchField = () => {
   return (
-    <div className="w-100 max-w-[480px] relative flex">
-      <div className="absolute top-[50%] left-[28px] sm:left-[30px]">
+    <div className="relative flex w-full mb-4 sm:mb-0">
+      <div className="absolute top-[41%] left-[12px]">
         <SearchIcon />
       </div>
       <input
         type="text"
         placeholder="Search for a country"
-        className="dark:bg-darkBlue searchField-input text-sm p-4 pl-9 w-11/12 rounded-md mt-4 mx-auto outline-none dark:text-white"
+        className="w-full max-w-[480px] !pl-9 inputs"
       />
     </div>
   );
