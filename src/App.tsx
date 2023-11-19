37691ac11db4b852aa14d37b9f230e30b9301590
Javier Guerrero <javigh87@gmail.com>
2023-11-19 09:51:49 +0100

    feature: Country handled from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import useAppStore from "./store/useAppStore";
-
-import { Country } from "./interfaces";
 
 import Layout from "./layout/Layout";
 
@@ -11,7 +9,7 @@ import { DetailPage, MainPage } from "./pages";
 import "./App.css";
 
 function App() {
-  const [country, setCountry] = useState<Country | null>(null);
+  const country = useAppStore((state) => state.countrySelected);
   const getCountries = useAppStore((state) => state.getCountries);
 
   useEffect(() => {
